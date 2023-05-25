ea07b74088039380d1a853b861f834c3e9aad65a
Luke Narke <lnarke@gmail.com>
2023-05-25 12:35:30 -0500

    daily update

--- pages/testing.js
+++ pages/testing.js
@@ -1,8 +1,16 @@
 import _ from "lodash"
 const test = "test"
 
-const group_By = (arr, fn) =>
-  arr.map(typeof fn === "function" ? fn : (val) => val[fn])
+const group_By = (arr, fn) => {
+  let arr1 = arr.map(typeof fn === "function" ? fn : (val) => val[fn])
+  let arr2 = arr1.reduce((acc, val, i) => {
+    acc[val] = (acc[val] || []).concat(arr[i])
+    console.log("acc[val]", acc[val])
+    console.log("arr[i]", arr[i])
+    return acc
+  }, {})
+  return arr2
+}
 
 console.log(group_By([6.1, 4.2, 6.3], Math.sqrt))
 console.log(group_By([6.1, 4.2, 6.3], Math.floor))
