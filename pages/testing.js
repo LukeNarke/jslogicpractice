50a74b25b2981f0bd9545590f56a3e004799962c
Luke Narke <lnarke@gmail.com>
2023-06-06 09:43:57 -0500

    more q and a's

--- pages/testing.js
+++ pages/testing.js
@@ -1,6 +1,18 @@
 import _ from "lodash"
 const test = "test"
 
+const mapValues = (obj, fn) =>
+  Object.keys(obj).reduce((acc, k) => {
+    acc[k] = fn(obj[k], k, obj)
+    return acc
+  }, {})
+
+const people = {
+  fred: { person: "fred", age: 40 },
+  pebbles: { person: "pebbles", age: 1 },
+}
+console.log(mapValues(people, (p) => p.age))
+
 function Testing() {
   return (
     <div>
