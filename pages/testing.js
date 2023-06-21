9b2e62f4b3c9671646eb47a1aa167bead1979216
Luke Narke <lnarke@gmail.com>
2023-06-21 09:19:52 -0500

    more q and as

--- pages/testing.js
+++ pages/testing.js
@@ -1,27 +1,10 @@
 import _ from "lodash"
 const test = "test"
 
-const reducedFilter = (data, keys, fn) =>
-  data.filter(fn).map((el) =>
-    keys.reduce((acc, key) => {
-      acc[key] = el[key]
-      return acc
-    }, {})
-  )
-const dataTwo = [
-  {
-    id: 1,
-    name: "john",
-    age: 24,
-  },
-  {
-    id: 2,
-    name: "mike",
-    age: 50,
-  },
-]
-
-console.log(reducedFilter(dataTwo, ["id", "name"], (item) => item.age > 24))
+const when = (pred, whenTrue) => (x) => pred(x) ? whenTrue(x) : x
+const doubleEvenNumbers = when((x) => x % 2 === 0, x * 2)
+console.log(doubleEvenNumbers(2))
+console.log(doubleEvenNumbers(1))
 
 function Testing() {
   return (
